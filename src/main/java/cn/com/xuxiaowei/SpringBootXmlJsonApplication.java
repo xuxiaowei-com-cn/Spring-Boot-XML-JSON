398cfb811dc2488b39829e3512514ce0e1ec9ab2
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2019-12-12 09:36:45 +0800

    Initial commit

---
+++ src/main/java/cn/com/xuxiaowei/SpringBootXmlJsonApplication.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootXmlJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootXmlJsonApplication.class, args);
+    }
+
+}
